7ad94ea47bdd6d930716b28a760946daf176c104
SteveMaranga01 <stevemaranga09@gmail.com>
2026-02-07 00:56:14 +0300

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import Hero from '@/sections/Hero'
+import Navbar from '@/components/Navbar'
+import About from '@/sections/About'
+import Contacts from '@/sections/Contacts'
+import Experience from '@/sections/Experience'
+import Projects from '@/sections/Projects'
+import Testimonials from '@/sections/Testimonials'
 
 function App() {
-  return 
-    <>
-      
-    </>
-  
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Contacts />
+        <Experience />
+        <Projects />
+        <Testimonials />
+      </main>
+    </div>
+  )
 }
 
 export default App
