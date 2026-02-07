f65bfac87b76d56668939f76119e91195126188a
SteveMaranga01 <stevemaranga09@gmail.com>
2026-02-07 22:40:51 +0300

    added to the folder

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section></section>
+    <section className='relative min-h-screen flex items-center overflow-hidden'>
+
+      {/*Bg*/}
+
+    <div className='absolute inset-0'>
+      <img src="/hero-bg.jpg" alt='Hero Image'
+      className='w-full h-full object-cover opacity-40'/>
+      <div className='absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background'></div>
+    </div>
+
+    {/*green dots*/}
+
+    <div className='absolute inset-0 overflow-hidden pointer-events-none'>
+      {[...Array(30)].map((_, i) => (
+        <div key={i} className='absolute w-1.5 h-1.5 rounded-full opacity-60' style={{
+            background: "#20B2A6",
+            left: `${Math.random() * 100}%`,
+            top: `${Math.random() * 100}%`,
+        }}/>
+      ))}
+    </div>
+    </section>
   )
 }
 
