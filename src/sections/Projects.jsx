1cb430b9954d5dd47037ac8217fc59840b918a11
SteveMaranga01 <stevemaranga09@gmail.com>
2026-02-09 00:19:47 +0300

    added

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,4 @@
-import { ArrowUpRight } from 'lucide-react';
+import { ArrowUpRight, Github } from 'lucide-react';
 import React from 'react'
 
 const projects = [
@@ -86,14 +86,47 @@ const Projects = () => {
 
                 {/* Overlay Links */}
 
-                <div>
-                  <a>
-                    <ArrowUpRight/>
+                <div className='absolute inset-0 flex items-center justify-center gap-4 
+                opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                  <a href={project.link} className='p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all'>
+                    <ArrowUpRight className='w-5 h-5'/>
                   </a>
+                  <a href={project.github} className='p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all'>
+                    <Github className='w-5 h-5'/>
+                  </a>
+                </div>
+              </div>
+
+              {/* Content */}
+
+              <div className='p-6 space-y-4'>
+                <div className='flex items-start justify-between'>
+                  <h3 className='text-xl font-semibold group-hover:text-primary transition-colors'>{project.title}</h3>
+                  <ArrowUpRight
+                  className='w-5 h-5 text-muted-foreground group-hover:text-primary group-hover:translate-x-1
+                  group-hover:-translate-y-1 transition-all'
+                  />
+                </div>
+                <p className='text-muted-foreground text-sm'>
+                  {project.description}
+                </p>
+                <div className='flex flex-wrap gap-2'>
+                  {project.tags.map((tag, tagIdx) =>(
+                  <span 
+                  key={tagIdx}
+                  className='px-4 py-1.5 rounded-full bg-surface text-xs font-medium 
+                  border border-border/50 text-muted-foreground hover:border-primary/50 hover:text-primary transition-all duration-300'>{tag}</span>
+                ))}
                 </div>
               </div>
             </div>
           ))}
+        </div>
+
+        {/* view All CTA */}
+
+        <div>
+          <AnimatedBorderButton
         </div>
       </div>
 
