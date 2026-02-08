4ff03b48a16642a74fc79efb9ff319d43ef05096
SteveMaranga01 <stevemaranga09@gmail.com>
2026-02-08 12:45:08 +0300

    updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -93,6 +93,23 @@ const Hero = () => {
         </div>
 
         {/* right column - profile image*/}
+
+        <div className='relative animate-fade-in animation-delay-300'>
+
+          {/* profile image*/}
+
+          <div className='relative max-w-md mx-auto'>
+            <div
+            className='absolute inset-0 rounded-3xl bg-gradient-to-br 
+            from-primary/30 via-transparent to-primary/10 blur-2xl animate-pulse'
+            />
+            <div className='relative glass rounded-3xl p-2 glow-border'>
+              <img src='/profile-photo.jpg' alt='Sam profile picture' className='w-full aspect-[4/5] object-cover rounded-2xl'/>
+            </div>
+          </div>
+
+
+        </div>
       </div>
     </div>
     </section>
